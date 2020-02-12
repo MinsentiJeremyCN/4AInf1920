fbbb8d74ef6be4df65f13d35e4e5ff07082b11e8
MinsentiJeremyCN <57175147+MinsentiJeremyCN@users.noreply.github.com>
2020-02-12 22:29:09 +0100

    Add files via upload

---
+++ MinsentiJeremyInformatica/Esercizi_IntelliJ/202_ThreadEquazione/src/it/itis/cuneo/Calcolo.java
@@ -0,0 +1,17 @@
+package it.itis.cuneo;
+
+
+public class Calcolo extends Thread {
+
+    private String nome;
+
+    public Calcolo(String nome) {
+        this.nome = nome;
+    }
+
+    public void run(){
+
+        Equazione.
+
+    }
+}
---
+++ MinsentiJeremyInformatica/Esercizi_IntelliJ/202_ThreadEquazione/src/it/itis/cuneo/Equazione.java
@@ -0,0 +1,55 @@
+package it.itis.cuneo;
+
+import java.lang.Math
+
+public class Equazione {
+
+    private int primaParentesi;
+    private int secondaParentesi;
+
+
+    public int getPrimaParentesi() {
+        return primaParentesi;
+    }
+
+    public void setPrimaParentesi(int primaParentesi) {
+        this.primaParentesi = primaParentesi;
+    }
+
+    public int getSecondaParentesi() {
+        return secondaParentesi;
+    }
+
+    public void setSecondaParentesi(int secondaParentesi) {
+        this.secondaParentesi = secondaParentesi;
+    }
+
+    public int parentesi1(){
+
+        int risolvo;
+
+        risolvo = 94 * -2;
+        risolvo = risolvo + 5;
+
+        return risolvo;
+
+    }
+
+    public int parentesi2(){
+
+        int risolvo2;
+
+        risolvo2 = 2 * -3;
+        risolvo2 = risolvo2 + 4;
+
+        return risolvo2;
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
